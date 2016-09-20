61cd92f7e72f0ec21f4cc8c82647f33a42f2a759
towaywu <bboyer@qq.com>
2016-09-20 22:14:08 +0800

    update dir

---
+++ javascript/jsbasic/02.js
@@ -0,0 +1,19 @@
+/**
+*  这个文件的作用 
+* 
+* @author will
+* @date 2016-04-25
+*/
+document.write("这是JS代码2");
+
+/**
+*  测试函数, 弹出一个test
+*  @author will
+*  @date 2016-04-25
+*  @update_date 2016-04-25
+*/
+function test() {	
+	alert("test"); //弹出框显示test;	
+}
+
+test(); // 执行
